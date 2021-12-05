b4a49cb9365e614f70bb5e0da381938c83b8e9d6
Rebecca Wardle <mattydwardle@gmail.com>
2021-12-05 06:23:24 +0000

    Working on day3 part 2, added gitgigore and removed binaries

--- day3.rs
+++ day3.rs
@@ -1,10 +1,13 @@
-fn main () {
+fn main() {
+    partTwo();
+}
+
+fn partOne() {
     let str_in: String = std::fs::read_to_string("./day3.txt").expect("Failed to read");
-    
+
     let mut bits: Vec<u32> = vec![0; 12];
     let mut thresh: u32 = 0;
 
-    
     for line in str_in.lines() {
         thresh += 1;
         for i in 0..line.len() {
@@ -19,17 +22,34 @@ fn main () {
     println!("thresh: {}", thresh);
     println!("bits: {:?}", bits);
     for i in 0..(bits.len() as u32) {
-
         println!("index: {}", bits[i as usize]);
 
         if bits[bits.len() - (1 + i as usize)] >= thresh {
             gamma += 1 << i;
-        }
-        else {
+        } else {
             epsilon += 1 << i;
         }
     }
     println!("gamma: {:05b}, {}", gamma, gamma);
     println!("epsilon: {:05b}, {}", epsilon, epsilon);
-    println!("product: {}", gamma*epsilon);
+    println!("product: {}", gamma * epsilon);
+}
+
+fn partTwo() {
+    let str_in: String = std::fs::read_to_string("./test.txt").expect("Failed to read");
+    let mut oxy: Vec<String> = str_in
+        .lines()
+        .map(|l| l.parse().unwrap())
+        .collect::<Vec<String>>();
+    let mut scrub = oxy.clone();
+
+    println!("oxy: {:?}", oxy);
+
+    for i in 0..oxy[0].len() {
+        let mut oxy_thresh = oxy.len() / 2;
+        let mut oxy_sum = 0;
+
+        let mut scrub_thesh = scrub.len() / 2;
+        let mut scrub_sum = 0;
+    }
 }
