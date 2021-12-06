24806aa82f65800a791a5f07d64e8c41c6dcc030
Rebecca Wardle <mattydwardle@gmail.com>
2021-12-06 02:25:46 +0000

    Done day3

--- day3.rs
+++ day3.rs
@@ -1,8 +1,9 @@
 fn main() {
-    partTwo();
+    part_one();
+    part_two();
 }
 
-fn partOne() {
+fn part_one() {
     let str_in: String = std::fs::read_to_string("./day3.txt").expect("Failed to read");
 
     let mut bits: Vec<u32> = vec![0; 12];
@@ -19,11 +20,7 @@ fn partOne() {
     let mut gamma: i32 = 0;
     let mut epsilon: i32 = 0;
 
-    println!("thresh: {}", thresh);
-    println!("bits: {:?}", bits);
     for i in 0..(bits.len() as u32) {
-        println!("index: {}", bits[i as usize]);
-
         if bits[bits.len() - (1 + i as usize)] >= thresh {
             gamma += 1 << i;
         } else {
@@ -35,21 +32,52 @@ fn partOne() {
     println!("product: {}", gamma * epsilon);
 }
 
-fn partTwo() {
-    let str_in: String = std::fs::read_to_string("./test.txt").expect("Failed to read");
-    let mut oxy: Vec<String> = str_in
-        .lines()
-        .map(|l| l.parse().unwrap())
-        .collect::<Vec<String>>();
-    let mut scrub = oxy.clone();
+fn part_tooer(mut oxy: Vec<String>, flag: bool) -> u32 {
+    let bit_length = oxy[0].len();
 
-    println!("oxy: {:?}", oxy);
+    for b in 0..bit_length {
+        if oxy.len() == 1 {
+            break;
+        }
 
-    for i in 0..oxy[0].len() {
-        let mut oxy_thresh = oxy.len() / 2;
-        let mut oxy_sum = 0;
+        let oxy_sum = oxy
+            .iter()
+            .filter(|num| num.chars().nth(b) == Some('1'))
+            .count();
 
-        let mut scrub_thesh = scrub.len() / 2;
-        let mut scrub_sum = 0;
+        let bit_threshold = (oxy.len() as f32 / 2.0).ceil() as usize;
+        let oxy_flag = if flag {
+            if oxy_sum >= bit_threshold {
+                '1'
+            } else {
+                '0'
+            }
+        } else {
+            if oxy_sum < bit_threshold {
+                '1'
+            } else {
+                '0'
+            }
+        };
+        oxy = oxy
+            .into_iter()
+            .filter(|o| o.chars().nth(b) != Some(oxy_flag))
+            .collect();
     }
+
+    return u32::from_str_radix(&oxy[0], 2).unwrap();
+}
+
+fn part_two() {
+    let oxy: Vec<String> = std::fs::read_to_string("./day3.txt")
+        .expect("Failed to read")
+        .lines()
+        .map(str::to_string)
+        .collect();
+    let scrub = oxy.clone();
+
+    println!(
+        "answer: {}",
+        part_tooer(oxy, true) * part_tooer(scrub, false)
+    );
 }
